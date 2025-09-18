d7be7d73bc3cef3f930b9aaf90bf2ea5d37e7261
Mher178 <mher.av20002@gmail.com>
2025-09-18 11:08:51 +0300

    Changed the code to create a conflict

--- Testing.py
+++ Testing.py
@@ -1,3 +1,12 @@
-print('Hello world')
-a = 5
-b = 7
+class Test:
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def new_function(self):
+        result = self.first + self.second
+        return result
+
+My_Test = Test(4,5)
+print(My_Test.new_function())
+k = 9
