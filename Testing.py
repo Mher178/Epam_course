891824a922e86c8c21232555993f345018a50b5f
Mher178 <mher.av20002@gmail.com>
2025-09-18 18:17:34 +0300

    Added new old_function

--- Testing.py
+++ Testing.py
@@ -6,7 +6,10 @@ class Test:
     def new_function(self):
         result = self.first + self.second
         return result
+    def old_function(self, vb1):
+        new_vb = vb1**2
+        return new_vb
 
 
 My_Test = Test(5,7)
-print(My_Test.new_function())
+print(My_Test.old_function(3))
